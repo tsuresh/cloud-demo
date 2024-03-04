7e867bc343a57827baf085fa89077f5c41865998
Suresh Peiris <tsmpmail@gmail.com>
2024-03-05 03:30:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello, IIT!\n");
+  res.end("Hello, IIT!\n CI/CD");
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
